47c56eeb00b349442342cdeeb5ee0ee807e3dffb
rorror45 <junseong.jang@columbia.edu>
2016-11-14 16:07:44 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -3,14 +3,9 @@
 """
 Columbia W4111 Intro to databases
 Example webserver
-
 To run locally
-
     python server.py
-
 Go to http://localhost:8111 in your browser
-
-
 A debugger such as "pdb" may be helpful for debugging.
 Read about it online.
 """
@@ -80,7 +75,6 @@ def before_request():
   This function is run at the beginning of every web request 
   (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request
-
   The variable g is globally accessible
   """
   try:
@@ -106,10 +100,7 @@ def teardown_request(exception):
 # @app.route is a decorator around index() that means:
 #   run index() whenever the user tries to access the "/" path using a GET request
 #
-# If you wanted the user to go to e.g., localhost:8111/foobar/ with 
-
-
-or GET then you could use
+# If you wanted the user to go to e.g., localhost:8111/foobar/ with POST or GET then you could use
 #
 #       @app.route("/foobar/", methods=["POST", "GET"])
 #
@@ -122,11 +113,9 @@ or GET then you could use
 def index():
   """
   request is a special object that Flask provides to access web request information:
-
   request.method:   "GET" or "POST"
   request.form:     if the browser submitted a form, this contains the data in the form
   request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
   See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
   """
 
@@ -219,13 +208,9 @@ if __name__ == "__main__":
     """
     This function handles command line parameters.
     Run the server using
-
         python server.py
-
     Show the help text using
-
         python server.py --help
-
     """
 
     HOST, PORT = host, port
