5e7002e4a8c7810c3f1895f713ce477c8eb5aabb
rorror45 <junseong.jang@columbia.edu>
2016-11-15 10:33:19 -0500

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -33,7 +33,7 @@ app.secret_key = 'secretkey'
 #     DATABASEURI = "postgresql://ewu2493:foobar@<IP_OF_POSTGRE_SQL_SERVER>/postgres"
 #
 # Swap out the URI below with the URI for the database created in part 2
-DATABASEURI = "postgresql://jj2882:4zpdd@104.196.166.54/postgres"
+DATABASEURI = "postgresql://jj2882:4zpdd@104.196.175.120/postgres"
 
 
 #
